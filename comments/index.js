f660fb2859ae821381e6af3db2eed816b635d8ed
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-05-17 11:42:37 +0530

    Status moderation updated along with front end

--- comments/index.js
+++ comments/index.js
@@ -21,7 +21,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 
   const comments = commentsByPostId[postId] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: "pending" });
 
   commentsByPostId[postId] = comments;
 
@@ -32,6 +32,7 @@ app.post("/posts/:id/comments", async (req, res) => {
         id: commentId,
         content,
         postId,
+        status: "pending",
       },
     })
     .catch((err) => console.log(err));
@@ -39,9 +40,28 @@ app.post("/posts/:id/comments", async (req, res) => {
   return res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Received Event: ", req.body.type);
+  const { type, data } = req.body;
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
 
+    const comment = comments.find((comment) => comment.id === id);
+    comment.status = status;
+
+    await axios
+      .post("http://localhost:4005/events", {
+        type: "CommentUpdated",
+        data: {
+          id,
+          postId,
+          status,
+          content,
+        },
+      })
+      .catch((err) => console.log(err));
+  }
   res.send({});
 });
 
--- moderation/index.js
+++ moderation/index.js
@@ -1,8 +1,30 @@
-const express = require('express')
-const axios = require("axios")
+const express = require("express");
+const axios = require("axios");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
 
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+    await axios
+      .post("http://localhost:4005/events", {
+        type: "CommentModerated",
+        data: {
+          id: data.id,
+          postId: data.postId,
+          status,
+          content: data.content,
+        },
+      })
+      .catch((err) => console.log(err));
+  }
+  res.send({});
+});
+
+app.listen(4003, () => {
+  console.log("Listening to port 4003");
+});
