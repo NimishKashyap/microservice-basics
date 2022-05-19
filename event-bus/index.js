d043673a6cbdc9fae2c6c48061c598d83c49df18
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-05-19 20:02:51 +0530

    K8 pods init

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,8 +4,16 @@ const app = express();
 
 app.use(express.json());
 
+const events = [];
+
+app.get("/events", (req, res) => {
+  res.send(events);
+});
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios
     .post("http://localhost:4000/events", event)
@@ -19,7 +27,6 @@ app.post("/events", (req, res) => {
   axios
     .post("http://localhost:4003/events", event)
     .catch((err) => console.log(err));
-
 
   res.send({ status: "OK" });
 });
