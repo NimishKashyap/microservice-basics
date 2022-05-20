7386b923188b39f8aa3bddf0ffe11283df38cea6
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-05-20 12:58:48 +0530

    ingress controller added | client deployment added

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -7,7 +7,7 @@ function PostCreate() {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post("http://localhost:4000/posts", { title });
+    await axios.post("http://posts.com/posts", { title });
 
     setTitle("");
   };
